35cdb1e47d7ef644ec030a656f69cc9cf9be6779
lena-kosa <mail4helen@gmail.com>
2024-04-17 18:17:13 +0300

    test added

--- src/main/java/com/ads/adserver/AdServerApplication.java
+++ src/main/java/com/ads/adserver/AdServerApplication.java
@@ -3,9 +3,11 @@ package com.ads.adserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
+@EnableScheduling
 public class AdServerApplication {
 
 	@Bean
---
+++ src/test/java/com/ads/adserver/service/AdServiceTest.java
@@ -0,0 +1,50 @@
+package com.ads.adserver.service;
+
+import com.ads.adserver.cache.LocalCache;
+import com.ads.adserver.domain.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+class AdServiceTest {
+
+    @Mock
+    private LocalCache localCache;
+
+    @InjectMocks
+    private AdService adService;
+
+    @Test
+    void createCampaign() {
+    }
+
+    @Test
+    void serveAd_MaxBidForCategory() {
+        String category = "phone";
+        Product product = new Product(1L, "Iphone 15", "phone", "abc", new BigDecimal("3500"));
+        Mockito.when(localCache.getMaxBidByCategory(category)).thenReturn(product);
+        Product result = adService.serveAd(category);
+        assertNotNull(result);
+        assertEquals(result, product);
+    }
+
+    @Test
+    void serveAd_MaxBid() {
+        String category = "gadget";
+        Product product = new Product(1L, "Iphone 15", "phone", "abc", new BigDecimal("3500"));
+        Mockito.when(localCache.getMaxBidByCategory(category)).thenReturn(null);
+        Mockito.when(localCache.getMaxBidProduct()).thenReturn(product);
+        Product result = adService.serveAd(category);
+        assertNotNull(result);
+        assertEquals(result, product);
+    }
+}
